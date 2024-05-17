7f590b48d2ed2e56a6d95981a9c7d31f6f45fa41
Jake Oh <jake.oh.lecture@gmail.com>
2024-05-17 12:05:51 +0900

    for, for-of, for-in

--- lab03_js/src/main/webapp/js/array.js
+++ lab03_js/src/main/webapp/js/array.js
@@ -22,8 +22,35 @@ output.innerHTML += html + '<br/>';
 
 // 자바 향상된 for 문장: for (변수 선언 : 배열) { ... }
 
+// for-of 문장: 배열의 원소(아이템)들을 iteration(순회).
 html = '';
 for (let val of arr) {
     html += `${val}, `;
 }
 output.innerHTML += html + '<br/>';
+
+// for-in 문장: 배열의 인덱스들을 iteration(순회).
+html = '';
+for (let idx in arr) {
+    html += `${idx} : ${arr[idx]}, `;
+}
+output.innerHTML += html + '<br/>';
+
+// 배열 arr의 원소들의 합계, 평균을 output에 출력.
+let sum = 0; // 배열 원소들의 합계를 저장할 변수.
+for (let val of arr) {
+    sum += val;
+}
+
+const avg = sum / arr.length; //-> / 연산자: 소수점이하 계산 수행.
+
+output.innerHTML += `sum = ${sum}, average = ${avg} <br/>`;
+
+// 배열의 원소들을 ul의 li 요소로 만듦:
+const drama = ['삼식이 삼촌', '동조자', '삼체'];
+const list = document.querySelector('ul#list');
+html = '';
+for (let val of drama) {
+    html += `<li> ${val} </li>`;
+}
+list.innerHTML = html;
