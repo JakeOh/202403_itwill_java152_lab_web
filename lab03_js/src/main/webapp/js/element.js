cee54a2d9b7058b2a22793c39cfa93b9e1d081f3
Jake Oh <jake.oh.lecture@gmail.com>
2024-05-20 16:25:13 +0900

    documents methods

--- lab03_js/src/main/webapp/js/element.js
+++ lab03_js/src/main/webapp/js/element.js
@@ -23,8 +23,50 @@ const btn2 = document.querySelector('button#btn2');
 // btn2에 클릭 이벤트 리스너를 설정.
 btn2.addEventListener('click', function() {
     // class 속성 값이 "c1"인 요소들의 바탕색을 변경.
-    const divs = document.getElementsByClassName('c1'); //-> 배열을 리턴.
-    console.log(divs);
+    const divs = document.getElementsByClassName('c1'); //-> HTMLCollection(배열과 비슷)을 리턴.
+//    console.log(divs);
+    
+    for (let e of divs) {
+        e.style.backgroundColor = 'tomato';
+    }
     
 });
 
+// button#btn3 요소 찾음:
+const btn3 = document.querySelector('button#btn3');
+
+// btn3에 클릭 이벤트 리스너를 설정:
+btn3.addEventListener('click', () => {
+    // 태그 이름이 div인 모든 요소들을 찾아서 바탕색을 변경
+    const divs = document.getElementsByTagName('div');
+//    console.log(divs);
+    
+    for (const e of divs) {
+        e.style.backgroundColor = 'slateblue';
+    }
+});
+
+// document.querySelector(), document.querySelectorAll():
+// CSS 선택자 문법으로 아규먼트를 전달.
+// tagname, #id, .classname, tagname#id.classname
+// parent > child
+// ancestor descendent
+// element:pseudo-selector
+
+const btn4 = document.querySelector('button#btn4');
+btn4.addEventListener('click', function() {
+    const div4 = document.querySelector('#id4'); // querySelector('div#id4')
+//    console.log(div4);
+    div4.style.backgroundColor = 'gray';
+});
+ 
+const btn5 = document.querySelector('button#btn5');
+btn5.addEventListener('click', () => {
+    const divs = document.querySelectorAll('div.c2'); //-> NodeList(배열과 비슷)를 리턴.
+//    console.log(divs);
+    
+//    for (let e of divs) {
+//        e.style.backgroundColor = 'dodgerblue';
+//    }
+    divs.forEach((x) => x.style.backgroundColor = 'violet');
+});
