224d341d18ca47c640a4ea782d95a939582493da
Jake Oh <jake.oh.lecture@gmail.com>
2024-05-20 17:31:12 +0900

    event listeners

--- lab03_js/src/main/webapp/js/event.js
+++ lab03_js/src/main/webapp/js/event.js
@@ -15,3 +15,15 @@ btnInput.addEventListener('click', function (e) {
     itemList.innerHTML += `<li> ${itemInput.value} </li>`;
     itemInput.value = '';
 });
+
+// TODO: input#itemInput2 요소에 'keydown' 이벤트 리스너를 등록:
+// 엔터키가 눌렸을 때, input에 입력된 내용을 ul#itemList2의 리스트 아이템으로 추가.
+
+// TODO: input#username 요소에 'change' 이벤트 리스너를 등록:
+// input에 입력된 내용이 바뀔 때마다 div를 입력 내용으로 덮어씀.
+
+// TODO: img#bulb 요소에 'mouseenter' 이벤트 리스너를 등록:
+// img의 src를 'images/bulb_on.gif'로 변경.
+
+// TODO: img#bulb 요소에 'mouseleave' 이벤트 리스너를 등록:
+// img의 src를 'images/bulb_off.gif'로 변경.
