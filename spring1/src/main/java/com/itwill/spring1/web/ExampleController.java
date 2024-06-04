b26825a13b8c425ab5e1bf66bef6aa1f95e4146e
Jake Oh <jake.oh.lecture@gmail.com>
2024-06-04 17:33:16 +0900

    spring web mvc

--- spring1/src/main/java/com/itwill/spring1/web/ExampleController.java
+++ spring1/src/main/java/com/itwill/spring1/web/ExampleController.java
@@ -1,6 +1,9 @@
 package com.itwill.spring1.web;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 public class ExampleController {
     
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
         log.debug("home()");
+        
+        LocalDateTime now = LocalDateTime.now();
+        model.addAttribute("now", now);
         
         return "home";
     }
--- spring1/src/main/webapp/WEB-INF/views/home.jsp
+++ spring1/src/main/webapp/WEB-INF/views/home.jsp
@@ -15,6 +15,7 @@
 <body>
     <header>
         <h1>Home</h1>
+        <h2>${now}</h2>
     </header>
     
     <main></main>
