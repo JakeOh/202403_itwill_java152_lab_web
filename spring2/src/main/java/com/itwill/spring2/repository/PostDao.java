3ec922c0f19c3be5626e9dd2ee8b42d5c7d695a7
Jake Oh <jake.oh.lecture@gmail.com>
2024-06-11 11:23:21 +0900

    MyBatis-Spring, mapper xml

--- spring2/src/main/java/com/itwill/spring2/repository/PostDao.java
+++ spring2/src/main/java/com/itwill/spring2/repository/PostDao.java
@@ -6,5 +6,7 @@ public interface PostDao {
 
     // post-mapper.xml에서 id="selectOrderByIdDesc"인 SQL을 실행하는 메서드.
     List<Post> selectOrderByIdDesc();
+    Post selectById(Integer id);
+    int insertPost(Post post);
     
 }
--- spring2/src/test/java/com/itwill/spring2/repository/PostDaoTest.java
+++ spring2/src/test/java/com/itwill/spring2/repository/PostDaoTest.java
@@ -18,10 +18,10 @@ import lombok.extern.slf4j.Slf4j;
 )
 public class PostDaoTest {
 
-    @Autowired
+    @Autowired // 의존성 주입(DI)
     private PostDao postDao;
     
-    @Test
+//    @Test
     public void testSelectAll() {
         Assertions.assertNotNull(postDao);
         
@@ -30,6 +30,28 @@ public class PostDaoTest {
             System.out.println("\t" + p);
         }
         
+    }
+    
+//    @Test
+    public void testSelectById() {
+        Post post1 = postDao.selectById(2); // DB 테이블에 id가 있는 경우
+        Assertions.assertNotNull(post1);
+        log.debug(post1.toString());
+        
+        Post post2 = postDao.selectById(1); // DB 테이블에 id가 없는 경우
+        Assertions.assertNull(post2);
+    }
+    
+    @Test
+    public void testInsert() {
+        // insert할 데이터
+        Post post = Post.builder()
+                .title("MyBatis 테스트")
+                .content("MyBatis-Spring insert 테스트")
+                .author("admin")
+                .build();
+        int result = postDao.insertPost(post);
+        Assertions.assertEquals(1, result);
     }
     
 }
