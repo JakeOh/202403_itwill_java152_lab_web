ddacce543ac5768b0072d27734409d6dc02340fc
Jake Oh <jake.oh.lecture@gmail.com>
2024-06-12 14:54:47 +0900

    .

--- spring2/src/main/java/com/itwill/spring2/dto/PostCreateDto.java
+++ spring2/src/main/java/com/itwill/spring2/dto/PostCreateDto.java
@@ -5,6 +5,7 @@ import com.itwill.spring2.repository.Post;
 import lombok.Data;
 
 @Data
+// 새 글 작성 요청에서 요청 파라미터들을 저장하기 위한 DTO
 public class PostCreateDto {
     // 필드 이름을 요청 파라미터 이름과 같게 선언 & 기본 생성자 & setter.
     private String title;
