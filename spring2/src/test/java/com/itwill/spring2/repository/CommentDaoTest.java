a18cbd40c97030fec45dd20ecf81a7596cc12354
Jake Oh <jake.oh.lecture@gmail.com>
2024-06-13 16:20:10 +0900

    comment mapper, interface

--- spring2/src/test/java/com/itwill/spring2/repository/CommentDaoTest.java
+++ spring2/src/test/java/com/itwill/spring2/repository/CommentDaoTest.java
@@ -34,17 +34,49 @@ public class CommentDaoTest {
 //    @Test
     public void testInsert() {
         Comment comment = Comment.builder()
-                .postId(102).username("admin").ctext("댓글 입력 테스트")
+                .postId(102).username("guest").ctext("댓글 놀이...")
                 .build();
         int result = commentDao.insert(comment);
         Assertions.assertEquals(1, result);
     }
     
-    @Test
+//    @Test
     public void testUpdate() {
         Comment comment = Comment.builder().id(3).ctext("update test ... ㅎㅎㅎ").build();
         int result = commentDao.update(comment);
         Assertions.assertEquals(1, result);
+    }
+    
+//    @Test
+    public void testDeleteById() {
+        int result = commentDao.deleteById(2);
+        Assertions.assertEquals(1, result);
+    }
+    
+//    @Test
+    public void testDeleteByPostId() {
+        int result = commentDao.deleteByPostId(102);
+        // 포스트 번호 102번에 댓글이 2개 있는 경우:
+        Assertions.assertEquals(2, result);
+    }
+    
+//    @Test
+    public void testCommentCount() {
+        Integer result = commentDao.selectCommentCount(102);
+        // 테이블에 댓글이 5개 있는 경우
+        Assertions.assertEquals(5, result);
+    }
+    
+    @Test
+    public void testSelectById() {
+        // 테이블에 댓글 아이디(PK)가 있는 경우
+        Comment comment1 = commentDao.selectById(9);
+        Assertions.assertNotNull(comment1);
+        log.debug(comment1.toString());
+        
+        // 테이블에 댓글 아이디가 없는 경우
+        Comment comment2 = commentDao.selectById(1);
+        Assertions.assertNull(comment2);
     }
     
 }
