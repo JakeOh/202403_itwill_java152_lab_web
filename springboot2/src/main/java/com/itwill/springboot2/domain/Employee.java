1b881d13183eafd9c085086d93ba8e96a26856e6
Jake Oh <jake.oh.lecture@gmail.com>
2024-07-19 16:26:42 +0900

    JpaRepository

--- springboot2/src/main/java/com/itwill/springboot2/domain/Employee.java
+++ springboot2/src/main/java/com/itwill/springboot2/domain/Employee.java
@@ -6,11 +6,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 // ORM(Object Relation Mapping) -> JPA(Java Persistence API) -> Hibernate
 
-@NoArgsConstructor
+@NoArgsConstructor @Getter @ToString @EqualsAndHashCode
 @Entity // 데이터베이스 테이블과 매핑하는 자바 객체
 @Table(name = "EMP") // 클래스 이름과 실제 테이블 이름이 다를 때. 
 public class Employee {
