edc907e57caa4df39b31f94c12d83368e4438653
Jake Oh <jake.oh.lecture@gmail.com>
2024-07-25 18:20:40 +0900

    employee, department details

--- springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
+++ springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
@@ -2,11 +2,18 @@ package com.itwill.springboot3.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.springboot3.domain.Department;
+import com.itwill.springboot3.domain.Employee;
+import com.itwill.springboot3.dto.DepartmentDetailsDto;
 import com.itwill.springboot3.repository.DepartmentRepository;
+import com.itwill.springboot3.repository.EmployeeRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,12 +24,27 @@ import lombok.extern.slf4j.Slf4j;
 public class DepartmentService {
     
     private final DepartmentRepository deptRepo;
+    private final EmployeeRepository empRepo;
     
     @Transactional(readOnly = true)
-    public List<Department> read() {
-        log.info("read()");
+    public Page<Department> read(int pageNo, Sort sort) {
+        log.info("read(pageNo={}, sort={})", pageNo, sort);
         
-        return deptRepo.findAll();
+        Pageable pageable = PageRequest.of(pageNo, 5, sort);
+        Page<Department> page = deptRepo.findAll(pageable);
+        
+        return page;
+    }
+    
+    @Transactional(readOnly = true)
+    public DepartmentDetailsDto read(Integer id) {
+        log.info("read(id={})", id);
+        
+        Department department = deptRepo.findById(id).orElseThrow();
+        List<Employee> employees = empRepo.findByDepartmentId(id);
+        log.info("# of employees = {}", employees.size());
+        
+        return DepartmentDetailsDto.fromEntity(department, employees);
     }
 
 }
--- springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
+++ springboot3/src/main/java/com/itwill/springboot3/web/DepartmentController.java
@@ -1,13 +1,16 @@
 package com.itwill.springboot3.web;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwill.springboot3.domain.Department;
+import com.itwill.springboot3.dto.DepartmentDetailsDto;
 import com.itwill.springboot3.service.DepartmentService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,11 +24,22 @@ public class DepartmentController {
     private final DepartmentService deptSvc;
     
     @GetMapping("/list")
-    public void list(Model model) {
-        log.info("list()");
+    public void list(@RequestParam(name = "p", defaultValue = "0") int pageNo,
+            Model model) {
+        log.info("list(pageNo={})", pageNo);
         
-        List<Department> list = deptSvc.read();
+        Page<Department> list = deptSvc.read(pageNo, Sort.by("id"));
         model.addAttribute("page", list);
+    }
+    
+    @GetMapping("/details/{id}")
+    public String details(@PathVariable Integer id, Model model) {
+        log.info("details(id={})", id);
+        
+        DepartmentDetailsDto dto = deptSvc.read(id);
+        model.addAttribute("department", dto);
+        
+        return "department/details";
     }
 
 }
