3f806c60787927d08c5fc1ae9f16f73b34a87bf8
Jake Oh <jake.oh.lecture@gmail.com>
2024-07-26 12:13:04 +0900

    find with example

--- springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
+++ springboot3/src/main/java/com/itwill/springboot3/service/DepartmentService.java
@@ -2,6 +2,7 @@ package com.itwill.springboot3.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -50,8 +51,20 @@ public class DepartmentService {
     @Transactional(readOnly = true)
     public DepartmentDetailsDto read(String departmentName) {
         log.info("read(departmentName={})", departmentName);
-        //TODO
-        return null;
+        
+        // Department 엔터티 객체 생성:
+        Department department = Department.builder().departmentName(departmentName).build();
+
+        // Example 객체 생성:
+        Example<Department> example = Example.of(department);
+        
+        // Example 객체를 findOne() 또는 findAll() 메서드의 아규먼트로 전달:
+        Department resultDept = deptRepo.findOne(example).orElseThrow();
+        log.info("resultDept id = {}", resultDept.getId());
+        
+        List<Employee> employees = empRepo.findByDepartmentId(resultDept.getId());
+        
+        return DepartmentDetailsDto.fromEntity(resultDept, employees);
     }
 
 }
