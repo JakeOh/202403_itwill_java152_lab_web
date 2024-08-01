892acafe5e10bf0e4c12382b007861735c2a8352
Jake Oh <jake.oh.lecture@gmail.com>
2024-08-01 17:22:02 +0900

    added QClasses

---
+++ springboot5/build/generated/querydsl/com/itwill/springboot5/domain/QPost.java
@@ -0,0 +1,51 @@
+package com.itwill.springboot5.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPost is a Querydsl query type for Post
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPost extends EntityPathBase<Post> {
+
+    private static final long serialVersionUID = 1066611182L;
+
+    public static final QPost post = new QPost("post");
+
+    public final QBaseTimeEntity _super = new QBaseTimeEntity(this);
+
+    public final StringPath author = createString("author");
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdTime = _super.createdTime;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedTime = _super.modifiedTime;
+
+    public final StringPath title = createString("title");
+
+    public QPost(String variable) {
+        super(Post.class, forVariable(variable));
+    }
+
+    public QPost(Path<? extends Post> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPost(PathMetadata metadata) {
+        super(Post.class, metadata);
+    }
+
+}
+
