95d7a3ead0dcf9fad023af696d8ee917bbc54104
Jake Oh <jake.oh.lecture@gmail.com>
2024-08-02 10:18:08 +0900

    Comments, REST, view, controller, service, repository

---
+++ springboot5/src/main/java/com/itwill/springboot5/service/CommentService.java
@@ -0,0 +1,17 @@
+package com.itwill.springboot5.service;
+
+import org.springframework.stereotype.Service;
+
+import com.itwill.springboot5.repository.CommentRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+    
+    private final CommentRepository commentRepo;
+
+}
---
+++ springboot5/src/main/resources/static/js/comments.js
@@ -0,0 +1,8 @@
+/**
+ * comments.js
+ * /post/details.html에 포함.
+ * 댓글 생성, 목록, 수정, 삭제.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    
+});
