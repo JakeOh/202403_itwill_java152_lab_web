71bfde3ccdfc1609f4dffa44d4747013a49d4e03
Jake Oh <jake.oh.lecture@gmail.com>
2024-08-02 11:15:06 +0900

    comments view, js

--- springboot5/src/main/resources/static/js/comments.js
+++ springboot5/src/main/resources/static/js/comments.js
@@ -4,5 +4,26 @@
  * 댓글 생성, 목록, 수정, 삭제.
  */
 document.addEventListener('DOMContentLoaded', () => {
+    // bootstrap 라이브러리의 Collapse 객체를 생성:
+    const bsCollapse = new bootstrap.Collapse('div#collapseComments', {toggle: false});
+    
+    // [댓글 보기] 버튼을 찾아서, 클릭 이벤트 리스너를 설정.
+    const btnToggle = document.querySelector('button#btnToggle');
+    btnToggle.addEventListener('click', () => {
+        bsCollapse.toggle(); // Collapse 객체를 보기/감추기 토글.
+        
+        const toggle = btnToggle.getAttribute('data-toggle');
+        if (toggle === 'collapse') {
+            btnToggle.innerHTML = '댓글 감추기';
+            btnToggle.setAttribute('data-toggle', 'unfold');
+        } else {
+            btnToggle.innerHTML = '댓글 보기';
+            btnToggle.setAttribute('data-toggle', 'collapse');
+        }
+    });
+    
+    // 댓글 [등록] 버튼을 찾아서, 클릭 이벤트 리스너를 설정:
+    const btnRegisterComment = document.querySelector('button#btnRegisterComment');
+    btnRegisterComment.addEventListener('click', () => {});
     
 });
